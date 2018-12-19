faba36e377c0e329ec95d5fc01ae65425e7ddf1e
BaranKazan <Baran.kazan@hotmail.com>
2018-12-19 20:42:19 +0100

    Successfully combined two android project!

--- kronoxApp/app/src/main/java/com/example/barankazan/kronoxapp/ScheduleFragment.java
+++ kronoxApp/app/src/main/java/com/example/barankazan/kronoxapp/ScheduleFragment.java
@@ -1,18 +1,98 @@
 package com.example.barankazan.kronoxapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class ScheduleFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter mAdapter;
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    CoordinatorLayout coordinatorLayout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        recyclerView = v.findViewById(R.id.recyclerView);
+        coordinatorLayout = v.findViewById(R.id.coordinatorLayout);
+
+        populateRecyclerView();
+        enableSwipeToDeleteAndUndo();
+
+        return v;
+    }
+
+    private void populateRecyclerView() {
+        stringArrayList.add("Item 1");
+        stringArrayList.add("Item 2");
+        stringArrayList.add("Item 3");
+        stringArrayList.add("Item 4");
+        stringArrayList.add("Item 5");
+        stringArrayList.add("Item 6");
+        stringArrayList.add("Item 7");
+        stringArrayList.add("Item 8");
+        stringArrayList.add("Item 9");
+        stringArrayList.add("Item 11");
+        stringArrayList.add("Item 12");
+        stringArrayList.add("Item 13");
+        stringArrayList.add("Item 14");
+        stringArrayList.add("Item 15");
+        stringArrayList.add("Item 16");
+        stringArrayList.add("Item 17");
+        stringArrayList.add("Item 18");
+        stringArrayList.add("Item 19");
+        stringArrayList.add("Item 20");
+        stringArrayList.add("Item 21");
+        stringArrayList.add("Item 22");
+        stringArrayList.add("Item 23");
+
+        mAdapter = new RecyclerViewAdapter(stringArrayList);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    private void enableSwipeToDeleteAndUndo() {
+
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(getActivity()) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+                final int position = viewHolder.getAdapterPosition();
+                final String item = mAdapter.getData().get(position);
+
+                mAdapter.removeItem(position);
+
+
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Item was removed from the list.", Snackbar.LENGTH_LONG);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        mAdapter.restoreItem(item, position);
+                        recyclerView.scrollToPosition(position);
+                    }
+                });
+
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
     }
 }
