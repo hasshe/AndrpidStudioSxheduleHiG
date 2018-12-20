e7e2a4d52256ac3731e792717643829a63bd2071
BaranKazan <Baran.kazan@hotmail.com>
2018-12-20 18:50:48 +0100

    Saving where I left of

--- Starbuzz/app/src/main/java/com/hfad/starbuzz/Drink.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/Drink.java
@@ -7,11 +7,7 @@ package com.hfad.starbuzz;
 public class Drink {
     private String name; private String description; private int imageResourceId;
     //drinks is an array of Drinks
-    public static final Drink[] drinks = {
-            new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
-            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
-            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
-    };
+
 
     //Each Drink has a name, description, and an image resource
     private Drink(String name, String description, int imageResourceId) {
