02766b395b39112e994df2e5ea52b79f28b7ac6c
hasshe <hassan.sheikha_97@outlook.com>
2018-12-31 15:56:29 +0100

    javadoc komment

--- kronoxApp/app/src/main/java/Parser/ScheduleInfo.java
+++ kronoxApp/app/src/main/java/Parser/ScheduleInfo.java
@@ -1,48 +1,93 @@
 package Parser;
 
+/**
+ * Modellen för vad som behövs för en lektion
+ */
+
 public class ScheduleInfo {
     private String courseCode, programCode, lectureInfo, start, stop, date,
-            startDate, startTime, stopTime, roomNr, teacherSignature, secondTeacherSignature;
+            startTime, stopTime, roomNr, teacherSignature, secondTeacherSignature;
 
+    /**
+     *
+     * @param courseCode sätter kursens kod
+     */
     public void setCourseCode(String courseCode) {
 
         this.courseCode = courseCode;
     }
+
+    /**
+     *
+     * @param programCode sätter programmets kod
+     */
     public void setProgramCode(String programCode) {
 
         this.programCode = programCode;
     }
+
+    /**
+     *
+     * @param lectureInfo sätter lektionens detaljerade information
+     */
     public void setLectureInfo(String lectureInfo) {
 
         this.lectureInfo = lectureInfo;
     }
+
+    /**
+     *
+     * @param roomNr sätter vilken sal lektionen tar plats i
+     */
     public void setRoomNr(String roomNr) {
 
         this.roomNr = roomNr;
     }
+
+    /**
+     *
+     * @param secondTeacherSignature sätter andra lektorns signatur ifall det finns
+     */
     public void setSecondTeacherSignature(String secondTeacherSignature) {
         this.secondTeacherSignature = secondTeacherSignature;
     }
+
+    /**
+     *
+     * @param teacherSignature sätter lektorns signatur
+     */
     public void setTeacherSignature(String teacherSignature) {
         this.teacherSignature = teacherSignature;
     }
+
+    /**
+     *
+     * @param date sätter datumet för lektionen
+     */
     public void setDate(String date) {
         this.date = date;
     }
 
+    /**
+     *
+     * @param start sätter tiden då lektionen startar
+     */
     public void setStart(String start) {
         this.start = start;
         setStartTime();
     }
 
+    /**
+     *
+     * @param stop sätter tiden då lektionen slutar
+     */
     public void setStop(String stop) {
         this.stop = stop;
         setStopTime();
     }
 
     /**
-     * Tar de relevanta delarna ifrån "DTSTART"-biten i schemat och
-     * lägger till ett kolon för att separera timme och minut.
+     * Här hanteras biten "DTSTART" och tar tiden i hh:mm
      */
     public void setStartTime() {
         int firstPart = Integer.parseInt(start.substring(start.lastIndexOf("T") + 1, start.lastIndexOf("Z")-4)) + 1;
@@ -50,42 +95,70 @@ public class ScheduleInfo {
     }
 
     /**
-     * Tar de relevanta delarna ifrån "DTSTOP"-biten i schemat och
-     * lägger till ett kolon för att separera timme och minut.
+     * Här hanteras biten "DTSTOP" och tar tiden i hh:mm
      */
     public void setStopTime() {
         int firstPart = Integer.parseInt(stop.substring(stop.lastIndexOf("T") + 1, stop.lastIndexOf("Z")-4)) + 1;
         stopTime = firstPart + ":" + stop.substring(stop.lastIndexOf("T") + 3, stop.lastIndexOf("Z")-2);
     }
+
+    /**
+     *
+     * @return detaljerad beskrivning om lektionen
+     */
     public String getLectureMoment () {
 
         return lectureInfo;
     }
 
+    /**
+     *
+     * @return datumet lektionen tar plats
+     */
     public String getDate() {
 
         return date;
     }
+
+    /**
+     *
+     * @return tiden lektionen startar
+     */
     public String getStartTime() {
 
         return startTime;
     }
+
+    /**
+     *
+     * @return tiden då lektionen slutar
+     */
     public String getStopTime() {
 
         return stopTime;
     }
+
+    /**
+     *
+     * @return koden till kursen
+     */
     public String getCourseCode() {
 
         return courseCode;
     }
-    public String getStart() {
-
-        return start;
-    }
+    /**
+     *
+     * @return salen där lektionen tar plats
+     */
     public String getRoomNr() {
 
         return roomNr;
     }
+
+    /**
+     *
+     * @return lektorns signatur
+     */
     public String getTeacherSignature() {
 
         return teacherSignature + " " + secondTeacherSignature;
